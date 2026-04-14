b844e2680bfc29b36b8ef23d17bca8e2bbf1b736
AdamT3H <adamogorodnik@gmail.com>
2026-04-14 10:48:51 +0300

    Added pixel

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
 
   return (
     <>
-      <FallingFood />
+      {/* <FallingFood /> */}
       <div className="container">
 
         <div className={styles.heroInner}>
